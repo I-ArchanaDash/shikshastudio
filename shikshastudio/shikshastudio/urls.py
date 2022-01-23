165213ad6f6f8eda1938ffb4f5522fcc2efbbe38
Archana Dash <iarchanadash@gmail.com>
2022-01-24 03:26:18 +0530

    fixed urls of shikshastudio

--- shikshastudio/shikshastudio/urls.py
+++ shikshastudio/shikshastudio/urls.py
@@ -19,6 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('',include('shikshapp.urls')),
     path('shikshapp/',include('shikshapp.urls')),
-    path('teacherDashboard/',include('teacherDashboard.urls')),
     path('admin/', admin.site.urls),
 ]
